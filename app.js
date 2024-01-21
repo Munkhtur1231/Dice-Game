f8893fd90782652f89bd24ab76f0ce1ed1e551c8
Munkhtur <Munkhtur1231@gmail.com>
2024-01-22 01:19:33 +0800

    1 sariin 21 ii ajil duuslaa

--- app.js
+++ app.js
@@ -8,24 +8,19 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Shoonii ali talaaraa buusniig hadgalah huvisagch, 1-6 gesen utgatai sanamsargui uusgene.
-var dice = Math.floor(Math.random()*6) + 1;
+
 
 // Program ehlehed beltgeh
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
-document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
 
-if (dice === 1) 
-    window.document.querySelector('.dice').outerHTML = '<img src="dice-1.png" alt="Dice" class="dice" />';
-if (dice === 2) 
-    window.document.querySelector('.dice').outerHTML = '<img src="dice-2.png" alt="Dice" class="dice" />';
-if (dice === 3)
-    window.document.querySelector('.dice').outerHTML = '<img src="dice-3.png" alt="Dice" class="dice" />';
-if (dice === 4) 
-    window.document.querySelector('.dice').outerHTML = '<img src="dice-4.png" alt="Dice" class="dice" />';
-if (dice === 5) 
-    window.document.querySelector('.dice').outerHTML = '<img src="dice-5.png" alt="Dice" class="dice" />';
-if (dice === 6) 
-    window.document.querySelector('.dice').outerHTML = '<img src="dice-6.png" alt="Dice" class="dice" />';
+var diceDom = document.querySelector('.dice');
+diceDom.style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    var diceNumber = Math.floor(Math.random()*6) + 1;
+    diceDom.style.display = 'block';
+    diceDom .src = 'dice-' + diceNumber + '.png';
+});
